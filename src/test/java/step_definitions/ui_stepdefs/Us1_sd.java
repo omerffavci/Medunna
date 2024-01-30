7325cb318a88d5b8447c38f1126448257ea9db11
Ömer Faruk AVCI <afaruk.avci@gmail.com>
2024-01-30 10:08:14 +0100

    Omer-30.01.2024.v2

--- src/test/java/step_definitions/ui_stepdefs/Us1_sd.java
+++ src/test/java/step_definitions/ui_stepdefs/Us1_sd.java
@@ -1,13 +1,13 @@
-package step_definitions.ui_stepdefs.stepdefs_Omer;
+package step_definitions.ui_stepdefs;
 
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
-import pages.us_1_2_3.RegisterPage;
+import pages.RegisterPage_omer;
 import utilities.ConfigReader;
 import utilities.Driver;
 
 public class Us1_sd {
-    RegisterPage registerPage = new RegisterPage();
+    RegisterPage_omer registerPage_omer = new RegisterPage_omer();
     @Given("Kullanici {string} ye giderr")
     public void kullaniciYeGiderr(String arg0) {
         Driver.getDriver().get(ConfigReader.getProperty("regurl"));
@@ -15,8 +15,8 @@ public class Us1_sd {
 
     @Then("Kullanici bütün bilgileri eksiksiz doldururr")
     public void kullaniciBütünBilgileriEksiksizDoldururr() {
-        registerPage.ssn_o.sendKeys("456-34-8658");
-        registerPage.firstname_o.sendKeys("Omer");
+        registerPage_omer.ssn_o.sendKeys("456-34-8658");
+        registerPage_omer.firstname_o.sendKeys("Omer");
 
     }
 
